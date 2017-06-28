0feae95ef9b4df9fd26adb9f129fb387e50c38ce
Joost P. de Vreede <entetex@live.nl>
2017-06-28 15:41:49 +0200

    Salt minion

---
+++ acceptSaltKeys.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+salt-key -A
--- main.sh
+++ main.sh
@@ -38,7 +38,7 @@ apt install salt-master -y
 # https://docs.saltstack.com/en/latest/ref/configuration/index.html#configuring-salt
 
 # Laat Salt via alle adressen luisteren
-sed 's/#interface: 0.0.0.0/interface: 0.0.0.0' /etc/salt/master
+sed 's/#interface: 0.0.0.0/interface: 0.0.0.0/' /etc/salt/master
 # Voeg de MASTER_ID toe, om naar de minions mee te geven
 echo "master_id: SaltMaster_$HOSTNAME" >> /etc/salt/master
 
@@ -52,5 +52,27 @@ systemctl start salt-master
 # Salt Minion
 # https://docs.saltstack.com/en/latest/topics/installation/ubuntu.html
 apt install salt-minion -y
+
+# Controleer het IP adres van de Salt Master
+SaltMasterIP="$(cat config.cfg | grep SaltMasterIP)"
+SaltMasterIP=${SaltMasterIP#SaltMasterIP=}
+LocalIP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
+if [ $SaltMasterIP -eq $LocalIP ]
+then
+    # De Minion wordt ingesteld op de SaltMaster
+    sed -i 's/#master: salt/master: 127.0.0.1/' /etc/salt/minion
+else
+    sed -i "s/#master: salt/master: $SaltMasterIP/" /etc/salt/minion
+fi
+
+# Stel de Master Key in
+SaltMasterKey="$(cat config.cfg | grep SaltMasterKey)"
+SaltMasterKey=${SaltMasterKey#SaltMasterKey=}
+sed -i "s/#master_finger: ''/master_finger: $SaltMasterKey/" /etc/salt/minion
+
+# Salt Minion als Daemon toevoegen
+systemctl enable salt-minion
+# Start Salt Minion
+systemctl start salt-minion
 
 # Start de server opnieuw op
